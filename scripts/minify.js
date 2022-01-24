d704133ff4aed148a3f50aab69b730296625f0ba
Jacob Sapoznikow <jacob1coder@gmail.com>
2022-01-24 00:29:16 +0000

    Skip minify

--- scripts/minify.js
+++ scripts/minify.js
@@ -27,22 +27,22 @@ async function main() {
         console.log(
             `[${i + 1}/${files.length}] Minifying ${files[i]} to ${files[i]}...`
         );
-        try {
-            await minify({
-                compressor: gcc,
-                input: folder + "/" + files[i],
-                output: minFolder + "/" + files[i],
-                options: {
-                    createSourceMap: true,
-                    compilationLevel: "WHITESPACE_ONLY",
-                    languageIn: "STABLE",
-                    languageOut: "STABLE",
-                },
-            });
-        } catch (e) {
-            console.error("An error occured: \n" + e);
-            process.exit(1);
-        }
+        // try {
+        //     await minify({
+        //         compressor: gcc,
+        //         input: folder + "/" + files[i],
+        //         output: minFolder + "/" + files[i],
+        //         options: {
+        //             createSourceMap: true,
+        //             compilationLevel: "WHITESPACE_ONLY",
+        //             languageIn: "STABLE",
+        //             languageOut: "STABLE",
+        //         },
+        //     });
+        // } catch (e) {
+        //     console.error("An error occured: \n" + e);
+        //     process.exit(1);
+        // }
         fs.copyFileSync(
             folder + "/" + files[i].replace(".js", ".d.ts"),
             minFolder + "/" + files[i].replace(".js", ".d.ts")
